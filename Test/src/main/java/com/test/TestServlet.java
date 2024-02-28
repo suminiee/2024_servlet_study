1014fd0f2f3230ac33994f54a742acc0eaabe27d
suminiee <chansu0114@naver.com>
2024-02-28 21:21:35 +0900

    print helloworld

--- Test/src/main/java/com/test/TestServlet.java
+++ Test/src/main/java/com/test/TestServlet.java
@@ -1,5 +1,18 @@
 package com.test;
 
-public class TestServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet{
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Hello world!");
+	}
+	
 }
